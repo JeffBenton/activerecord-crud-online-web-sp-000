140f3f87c9e0fafb8735c4d551e7a7bc3aceab88
Jeff Benton <sikletz@gmail.com>
2019-04-23 07:03:48 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -76,8 +76,9 @@ end
 def can_update_using_update_method
   # Update movie title to "Wat, huh?"
   Movie.create(title: "Wat?")
-  Movie.find_by(title: "Wat?").update(title: "huh?")
-  
+  movie = Movie.find_by(title: "Wat?").update(title: "huh?")
+  binding.pry
+  movie
 end
 
 def can_update_multiple_items_at_once
